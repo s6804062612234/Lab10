a218f4c7e5fef2d9cbc7e64766a7fecf0cd80684
Thananon Sananrum <s6804062612234@email.kmutnb.ac.th>
2026-02-24 14:06:32 +0700

    lab10no2.c

--- lab10.c
+++ lab10.c
@@ -2,37 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct {
-	char name[20];
-	int score;
-} Student;
+struct studentRecord {
+	char name[64];
+	int age;
+};
 
-void addStudent(Student *record, char *name, int score);
+void displayRecord(struct studentRecord student);
+void addAge(struct studentRecord *student, int age);
 
-int main() {
-	Student stuRecord[20];
-	int numStudent, i, j;
-	
-	addStudent(&stuRecord[0], "Sandking", 65);
-	addStudent(&stuRecord[1], "Anti-mage", 72);
-	addStudent(&stuRecord[2], "Sniper", 40);
-	addStudent(&stuRecord[3], "Tiny", 93); 
-	addStudent(&stuRecord[4], "Axe", 60);
-	numStudent = 5;
-	
-	for(i = 0; i < numStudent; i++) {
-		if(stuRecord[i].score >= 80) {
-			printf("%s %d very good\n", stuRecord[i].name, stuRecord[i].score);
-		} else if(stuRecord[i].score >= 60) {
-			printf("%s %d good\n", stuRecord[i].name, stuRecord[i].score);
-		} else {
-			printf("%s %d keep trying\n", stuRecord[i].name, stuRecord[i].score);
-		}
+int main () {
+	struct studentRecord student[5];
+	char tempName[64]; int i;
+	for(i = 0; i < 5; i++) {
+		sprintf(tempName, "Student%d", i);
+		strcpy(student[i].name, tempName);
+		student[i].age = i+15;
 	}
+	for(i = 0; i < 5; i++)
+		addAge(&student[i], 10);
+	for(i = 0; i < 5; i++)
+		displayRecord(student[i]);
 	return 0;
 }
 
-void addStudent(Student *record, char *name, int score) {
-	strcpy(record->name, name); 
-	record->score = score;
+void displayRecord(struct studentRecord student) {
+	printf("Name : %s, Age : %d\n", student.name, student.age);
 }
+
+void addAge(struct studentRecord *student, int age) {
+	student->age += age;
+}
+
